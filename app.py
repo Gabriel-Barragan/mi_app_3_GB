51e4085b8e78f5b22fa92a3bd8d5bd7204ae4e0e
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-31 09:01:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ Data = pd.read_csv('Data/california_housing_train.csv')
 # Create app
 st.title('California Housing Data')
 
+# Display summary
+st.write('Descriptive summary:')
+st.dataframe(Data.describe())
+
 # Display correlation matrix
 st.write('Correlation matrix:')
 st.dataframe(Data.corr())
