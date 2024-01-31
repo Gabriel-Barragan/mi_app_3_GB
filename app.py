a57c8cb89b75299bf1a3793e2e58188cd4b6f223
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-31 11:06:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ st.sidebar.write("## Data analysis :bar_chart:")
 # Load Data
 #Data = pd.read_csv('Data/california_housing_train.csv')
 Uploaded_file = st.sidebar.file_uploader('Upload Data:', type =['csv'])
-
 if Uploaded_file is not None:
   Data = pd.read_csv(Uploaded_file)
 
   # Create app
-  st.title('California Housing Data')
+  st.title('Base de datos')
 
+tab_titles = ['Data summary','Graphics']
+tabs = st.tabs(tab_titles)
+
+with tabs[0]:
   # Display summary
   st.write('Descriptive summary:')
   st.dataframe(Data.describe())
@@ -23,4 +26,6 @@ if Uploaded_file is not None:
   st.write('Correlation matrix:')
   st.dataframe(Data.corr())
 
+with tabs[1]:
+  st.write('Chart')
 
