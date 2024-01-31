61017a781a474efa5436dc921f325025d88df62b
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-31 12:42:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,9 @@ with tabs[1]:
   column = st.selectbox("Variable:", list(Data.columns))
 
   # Create a histogram with custom color and title
+  fig, ax = plt.subplots()
   plt.hist(Data[column], bins=20, color='skyblue', edgecolor='black')
   plt.title('My Custom Histogram')
  
   # Display the plot in Streamlit
-  st.pyplot()
+  st.pyplot(fig)
