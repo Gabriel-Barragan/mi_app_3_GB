a04906e7109b18d52d8771134b1a5b51cb0ef6aa
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-02 11:41:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import LinearRegression from sklearn.linear_model 
 
 # Title
 st.title('Exploratory Data Analysis')
